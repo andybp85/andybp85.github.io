904949b2b500806c73e0d0388e43e046c51dcbe6
Andrew Stanish <andybp85@gmail.com>
2019-11-23 14:01:56 -0500

    music page and ProjectCard

---
+++ sapper/src/components/ProjectCard.svelte
@@ -0,0 +1,56 @@
+<script>
+    export let title;
+    export let graphic;
+    export let blurb;
+    export let link;
+</script>
+
+<style lang="scss">
+    section {
+        display: grid;
+        grid-column-gap: 15px;
+        grid-template-areas: "title title"
+                             "blurb thumb"
+                             "link link";
+
+        h1 {
+            grid-area: title;
+        }
+
+        p {
+            grid-area: blurb;
+        }
+
+        img {
+            grid-area: thumb;
+            margin-top: 20px;
+        }
+
+        footer {
+            grid-area: link;
+        }
+
+    }
+
+</style>
+
+<section>
+
+    <h1>{title}</h1>
+
+    <p>
+        {@html blurb}
+    </p>
+
+    {#if graphic}
+    <img alt={title} src={graphic} />
+    {/if}
+
+
+    {#if link}
+    <footer>
+        <a href={link}>More</a>
+    </footer>
+    {/if}
+
+</section>
---
+++ sapper/src/routes/music.svelte
@@ -0,0 +1,26 @@
+<script>
+    import ProjectCard from '../components/ProjectCard.svelte';
+</script>
+
+<svelte:head>
+    <title>Music</title>
+</svelte:head>
+
+<ProjectCard
+    title="PandaJAM from Hamsterdam"
+    graphic="pjhd_flyer.jpg"
+    blurb="I helped run an open jam in Bergen County, NJ from 2012 to 2018. At its peak, we were drawing 10-30
+            musicians and 30-50 people every Wednesday. This was one of the best things I've ever done. Almost every
+            week, someone would come up to me and say, &ldquo;This is the only place I get to play with other people, thank
+            you so much for doing this.&rdquo; The jams we recorded are up on archive.org, but I made a website just for it:
+            <a href='https://www.pandajamfromhamsterdam.com'>pandajamfromhamsterdam.com</a>."
+/>
+
+<ProjectCard
+        title="PandaJAM (the band)"
+        graphic="PandaJAM-LMA-logo.jpg"
+        blurb='My old instrumental jamtronica band, in operation for four amazing years from 2013 to 2017. We formed
+                out of the open jam, and were heavily influenced by The Disco Biscuits, as well as funk and a bunch of
+                other stuff. Check out the live stuff we have up at
+                <a href="https://archive.org/details/PandaJAM">archive.org</a>'
+/>
